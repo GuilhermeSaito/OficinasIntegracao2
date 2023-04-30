f2e887c6f0ef93c9b36ba846395b2b13dd6f43ff
GuilhermeSaito <guilhermetoshiosaito@gmail.com>
2023-04-30 18:22:55 -0300

    Fixed frufrus dos labels, Added link para outra aba

--- app_mobile/hanbaiki/lib/main.dart
+++ app_mobile/hanbaiki/lib/main.dart
@@ -30,8 +30,6 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("Hello Another World"),
-              SizedBox(height: 20),
               LoginPage(),
             ],
           ),
@@ -41,6 +39,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ---------------------------------- LOGUIN PAGE
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -59,6 +58,7 @@ class _LoginPageState extends State<LoginPage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          // ------------ Container do email, senha e "Nao tem Loguin cadastre-se"
           Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.0),
@@ -72,59 +72,112 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ],
             ),
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'E-MAIL',
-                    // Da pra colocar uns frurus aqui
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(45.0),
+                    color: Colors.pink[100],
                   ),
-                  onSaved: (String? value) {
-                    _email = value;
-                  },
-                  validator: (String? value) {
-                    return (value != null && value.contains('@'))
-                        ? 'Do not use the @ char.'
-                        : null;
+                  width: MediaQuery.of(context).size.width * 2 / 3,
+                  // ---------------------- EMAIL BOX
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'E-MAIL',
+                      labelStyle: TextStyle(
+                        color:
+                            Colors.white, // set the color of the label to white
+                      ),
+                      border: InputBorder.none,
+                      // Da pra colocar uns frurus aqui
+                    ),
+                    onSaved: (String? value) {
+                      _email = value;
+                    },
+                    validator: (String? value) {
+                      return (value != null && value.contains('@'))
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.pink[100],
+                  ),
+                  width: MediaQuery.of(context).size.width * 2 / 3,
+                  // ---------------------- PASSWORD BOX
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'SENHA',
+                      labelStyle: TextStyle(
+                        color:
+                            Colors.white, // set the color of the label to white
+                      ),
+                      border: InputBorder.none,
+                    ),
+                    obscureText: true,
+                    // validator: (value) {
+                    //   if (value == null || value.isEmpty) {
+                    //     return 'Please enter your password';
+                    //   } else if (value.length < 8) {
+                    //     return 'Password must be at least 8 characters long';
+                    //   }
+                    //   return null;
+                    // },
+                    validator: (String? value) {
+                      return (value != null && value.contains('@'))
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                    onSaved: (String? value) {
+                      _password = value;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                // ---------------------- CONFIRM BUTTON
+                ElevatedButton(
+                  child: const Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink[100],
+                  ),
+                  onPressed: () {
+                    final form = _formKey.currentState;
+                    if (form != null) {
+                      form.save();
+                      print('Email: $_email');
+                      print('Password: $_password');
+                      // TODO: Implement login logic here
+                    }
                   },
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'SENHA',
+                SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    // navigate to another page
+                  },
+                  child: Text(
+                    'NÃO TEM LOGUIN? CADASTRE-SE',
+                    style: TextStyle(
+                      color: Colors
+                          .pink[200], // set the color of the text to light pink
+                    ),
                   ),
-                  obscureText: true,
-                  // validator: (value) {
-                  //   if (value == null || value.isEmpty) {
-                  //     return 'Please enter your password';
-                  //   } else if (value.length < 8) {
-                  //     return 'Password must be at least 8 characters long';
-                  //   }
-                  //   return null;
-                  // },
-                  validator: (String? value) {
-                    return (value != null && value.contains('@'))
-                        ? 'Do not use the @ char.'
-                        : null;
-                  },
-                  onSaved: (String? value) {
-                    _password = value;
-                  },
                 ),
               ],
             ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            child: const Text('Login'),
-            onPressed: () {
-              final form = _formKey.currentState;
-              if (form != null) {
-                form.save();
-                print('Email: $_email');
-                print('Password: $_password');
-                // TODO: Implement login logic here
-              }
-            },
           ),
         ],
       ),
