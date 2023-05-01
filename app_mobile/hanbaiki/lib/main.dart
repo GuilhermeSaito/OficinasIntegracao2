dd39447053c5efb95c79effd2055f3b6a1d25d95
GuilhermeSaito <guilhermetoshiosaito@gmail.com>
2023-05-01 20:27:36 -0300

    Added background image na tela de login, Fixed opacidade entre cada elemento da tela de login

--- app_mobile/hanbaiki/lib/main.dart
+++ app_mobile/hanbaiki/lib/main.dart
@@ -53,133 +53,157 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          // ------------ Container do email, senha e "Nao tem Loguin cadastre-se"
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(45.0),
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.pink.withOpacity(0.2),
+        image: const DecorationImage(
+          image: AssetImage("assets/back_ground.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Opacity(
+        opacity: 0.8,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Transform.translate(
+                offset: Offset(0, -100),
+                child: Text(
+                  "HA N BA I KI",
+                  style: TextStyle(
                     color: Colors.pink[100],
+                    fontSize: 70,
                   ),
-                  width: MediaQuery.of(context).size.width * 2 / 3,
-                  // ---------------------- EMAIL BOX
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'E-MAIL',
-                      labelStyle: TextStyle(
-                        color:
-                            Colors.white, // set the color of the label to white
+                ),
+              ),
+              // ------------ Container do email, senha e "Nao tem Loguin cadastre-se"
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(45.0),
+                        color: Colors.pink[100],
                       ),
-                      border: InputBorder.none,
-                      // Da pra colocar uns frurus aqui
-                    ),
-                    onSaved: (String? value) {
-                      _email = value;
-                    },
-                    validator: (String? value) {
-                      return (value != null && value.contains('@'))
-                          ? 'Do not use the @ char.'
-                          : null;
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Colors.pink[100],
-                  ),
-                  width: MediaQuery.of(context).size.width * 2 / 3,
-                  // ---------------------- PASSWORD BOX
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'SENHA',
-                      labelStyle: TextStyle(
-                        color:
-                            Colors.white, // set the color of the label to white
+                      width: MediaQuery.of(context).size.width * 2 / 3,
+                      // ---------------------- EMAIL BOX
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'E-MAIL',
+                          labelStyle: TextStyle(
+                            color: Colors
+                                .white, // set the color of the label to white
+                          ),
+                          border: InputBorder.none,
+                          // Da pra colocar uns frurus aqui
+                        ),
+                        onSaved: (String? value) {
+                          _email = value;
+                        },
+                        validator: (String? value) {
+                          return (value != null && value.contains('@'))
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
                       ),
-                      border: InputBorder.none,
                     ),
-                    obscureText: true,
-                    // validator: (value) {
-                    //   if (value == null || value.isEmpty) {
-                    //     return 'Please enter your password';
-                    //   } else if (value.length < 8) {
-                    //     return 'Password must be at least 8 characters long';
-                    //   }
-                    //   return null;
-                    // },
-                    validator: (String? value) {
-                      return (value != null && value.contains('@'))
-                          ? 'Do not use the @ char.'
-                          : null;
-                    },
-                    onSaved: (String? value) {
-                      _password = value;
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                // ---------------------- CONFIRM BUTTON
-                ElevatedButton(
-                  child: const Text('Login'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.pink[100],
-                  ),
-                  onPressed: () {
-                    final form = _formKey.currentState;
-                    if (form != null) {
-                      form.save();
-                      print('Email: $_email');
-                      print('Password: $_password');
-                      // TODO: Implement login logic here
-                    }
-                  },
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    // navigate to another page
-                  },
-                  child: Text(
-                    'NÃO TEM LOGUIN? CADASTRE-SE',
-                    style: TextStyle(
-                      color: Colors
-                          .pink[200], // set the color of the text to light pink
+                    SizedBox(
+                      height: 20.0,
                     ),
-                  ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.pink[100],
+                      ),
+                      width: MediaQuery.of(context).size.width * 2 / 3,
+                      // ---------------------- PASSWORD BOX
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'SENHA',
+                          labelStyle: TextStyle(
+                            color: Colors
+                                .white, // set the color of the label to white
+                          ),
+                          border: InputBorder.none,
+                        ),
+                        obscureText: true,
+                        // validator: (value) {
+                        //   if (value == null || value.isEmpty) {
+                        //     return 'Please enter your password';
+                        //   } else if (value.length < 8) {
+                        //     return 'Password must be at least 8 characters long';
+                        //   }
+                        //   return null;
+                        // },
+                        validator: (String? value) {
+                          return (value != null && value.contains('@'))
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
+                        onSaved: (String? value) {
+                          _password = value;
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    // ---------------------- CONFIRM BUTTON
+                    ElevatedButton(
+                      child: const Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink[100],
+                      ),
+                      onPressed: () {
+                        final form = _formKey.currentState;
+                        if (form != null) {
+                          form.save();
+                          print('Email: $_email');
+                          print('Password: $_password');
+                          // TODO: Implement login logic here
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        // navigate to another page
+                      },
+                      child: Text(
+                        'NÃO TEM LOGUIN? CADASTRE-SE',
+                        style: TextStyle(
+                          color: Colors.pink[
+                              200], // set the color of the text to light pink
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
